065951b67d64013c771acbab9f1546d4fc7b9a65
Sarthak Srivastava <sarthak.btech.ec17@iiitranchi.ac.in>
2020-12-30 18:48:07 +0530

    Update Binarytree2.py

--- Binarytree2.py
+++ Binarytree2.py
@@ -297,6 +297,44 @@ def getMinAndMax(root) :
 
 
 ################ Level order traversal
+
+# Level order traversal
+# Send Feedback
+# For a given a Binary Tree of type integer, print it in a level order fashion where each level will be printed on a new line. Elements on every level will be printed in a linear fashion and a single space will separate them.
+# Example:
+# alt txt
+
+# For the above-depicted tree, when printed in a level order fashion, the output would look like:
+
+# 10
+# 20 30 
+# 40 50 60
+# Where each new line denotes the level in the tree.
+# Input Format:
+# The first and the only line of input will contain the node data, all separated by a single space. Since -1 is used as an indication whether the left or right node data exist for root, it will not be a part of the node data.
+# Output Format:
+# The given input tree will be printed in a level order fashion where each level will be printed on a new line. 
+# Elements on every level will be printed in a linear fashion. A single space will separate them.
+# Constraints:
+# 1 <= N <= 10^5
+# Where N is the total number of nodes in the binary tree.
+
+# Time Limit: 1 sec
+# Sample Input 1:
+# 10 20 30 40 50 -1 60 -1 -1 -1 -1 -1 -1 
+# Sample Output 1:
+# 10 
+# 20 30 
+# 40 50 60 
+# Sample Input 2:
+# 8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+# Sample Output 2:
+# 8 
+# 3 10 
+# 1 6 14 
+# 4 7 13 
+
+
 def printLevelATNewLine(root):
     q = queue.Queue()
     if root == None:
@@ -320,6 +358,70 @@ def printLevelATNewLine(root):
 
 
 ########### Path Sum Root to Leaf
+
+# Path Sum Root to Leaf
+# Send Feedback
+# For a given Binary Tree of type integer and a number K, print out all root-to-leaf paths where the sum of all the node data along the path is equal to K.
+# Example:
+# alt txt
+
+# If you see in the above-depicted picture of Binary Tree, we see that there are a total of two paths, starting from the root and ending at the leaves which sum up to a value of K = 13.
+
+# The paths are:
+# a. 2 3 4 4
+# b. 2 3 8
+
+# One thing to note here is, there is another path in the right sub-tree in reference to the root, which sums up to 13 but since it doesn't end at the leaf, we discard it.
+# The path is: 2 9 2(not a leaf)
+#  Input Format:
+# The first line of input will contain the node data, all separated by a single space. Since -1 is used as an indication whether the left or right node data exist for root, it will not be a part of the node data.
+
+# The second line of input contains an integer value K.
+# Output Format:
+# Lines equal to the total number of paths will be printed. All the node data in every path will be printed in a linear fashion taken in the order they appear from top to down bottom in the tree. A single space will separate them all.
+# Constriants:
+# 1 <= N <= 10^5
+# 0 <= K <= 10^8
+# Where N is the total number of nodes in the binary tree.
+
+# Time Limit: 1 second
+# Sample Input 1:
+# 2 3 9 4 8 -1 2 4 -1 -1 -1 6 -1 -1 -1 -1 -1
+# 13
+#  Sample Output 1:
+# 2 3 4 4 
+# 2 3 8
+# Sample Input 2:
+# 5 6 7 2 3 -1 1 -1 -1 -1 9 -1 -1 -1 -1
+# 13
+#  Sample Output 2:
+# 5 6 2
+# 5 7 1
+
+# def rootToLeafPathsSumToK(root, k, lst):
+#     if root is None:
+#         return None
+#     if root.left is None and root.right is None:
+#         if root.data==k:
+#             print(*lst,k)
+#     lst.append(root.data)
+#     rootToLeafPathsSumToK(root.left,k-root.data,lst)
+#     rootToLeafPathsSumToK(root.right,k-root.data,lst)
+#     lst.pop()
+
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+
+#Following is the structure used to represent the Binary Tree Node
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
 def rootToLeafPathsSumToK(root, k, lst):
     if root is None:
         return None
@@ -330,7 +432,74 @@ def rootToLeafPathsSumToK(root, k, lst):
     rootToLeafPathsSumToK(root.left,k-root.data,lst)
     rootToLeafPathsSumToK(root.right,k-root.data,lst)
     lst.pop()
-         
+
+
+#Taking level-order input using fast I/O method
+def takeInput():
+    levelOrder = list(map(int, stdin.readline().strip().split(" ")))
+    start = 0
+    
+    length = len(levelOrder)
+
+    if length == 1 :
+        return None
+    
+    root = BinaryTreeNode(levelOrder[start])
+    start += 1
+
+    q = queue.Queue()
+    q.put(root)
+
+    while not q.empty():
+        currentNode = q.get()
+
+        leftChild = levelOrder[start]
+        start += 1
+
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+
+        rightChild = levelOrder[start]
+        start += 1
+
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+
+    return root
+
+    
+def printLevelWise(root):
+    if root is None:
+        return
+
+    inputQ = queue.Queue()
+    outputQ = queue.Queue()
+    inputQ.put(root)
+
+    while not inputQ.empty():
+       
+        while not inputQ.empty():
+       
+            curr = inputQ.get()
+            print(curr.data, end=' ')
+            if curr.left!=None:
+                outputQ.put(curr.left)
+            if curr.right!=None:
+                outputQ.put(curr.right)
+       
+        print()
+        inputQ, outputQ = outputQ, inputQ
+
+
+# Main
+root = takeInput()
+k = int(stdin.readline().strip())
+rootToLeafPathsSumToK(root, k,[])
+
   
   ##########has sum or not
 class Solution:
@@ -352,6 +521,45 @@ class Solution:
                 
   
 ######### Print nodes at distance k from node  
+
+
+# Print nodes at distance k from node
+# Send Feedback
+# You are given a Binary Tree of type integer, a target node, and an integer value K.
+# Print the data of all nodes that have a distance K from the target node. The order in which they would be printed will not matter.
+# Example:
+# For a given input tree(refer to the image below):
+# 1. Target Node: 5
+# 2. K = 2
+# alt txt
+
+# Starting from the target node 5, the nodes at distance K are 7 4 and 1.
+# Input Format:
+# The first line of input will contain the node data, all separated by a single space. Since -1 is used as an indication whether the left or right node data exist for root, it will not be a part of the node data.
+
+# The second line of input contains two integers separated by a single space, representing the value of the target node and K, respectively.
+# Output Format:
+# All the node data at distance K from the target node will be printed on a new line.
+
+# The order in which the data is printed doesn't matter.
+# Constraints:
+# 1 <= N <= 10^5
+# Where N is the total number of nodes in the binary tree.
+
+# Time Limit: 1 sec
+# Sample Input 1:
+# 5 6 10 2 3 -1 -1 -1 -1 -1 9 -1 -1
+# 3 1
+# Sample Output 1:
+# 9
+# 6
+# Sample Input 2:
+# 1 2 3 4 5 6 7 -1 -1 -1 -1 -1 -1 -1 -1
+# 3 3
+# Sample Output 2:
+# 4
+# 5
+
   
 def nodesAtDistanceKHelper(root, target, k) :
     if root is None :
