a3f176ac6670c1e5a954eefafc8a1171bce49d28
Sarthak Srivastava <sarthak.btech.ec17@iiitranchi.ac.in>
2020-12-31 08:51:27 +0530

    Update recurrsion3.py

--- recurrsion3.py
+++ recurrsion3.py
@@ -45,6 +45,80 @@ for ele in ans:
     print(ele)
 
 
+Return Keypad
+Send Feedback
+Given an integer n, using phone keypad find out all the possible strings that can be made using digits of input n.
+Note : The order of strings are not important.
+Input Format :
+Integer n
+Output Format :
+All possible strings in different lines
+Constraints :
+1 <= n <= 10^6
+Sample Input:
+23
+Sample Output:
+ad
+ae
+af
+bd
+be
+bf
+cd
+ce
+cf
+    
+    
+def getString(d):
+    if d == 2:
+        return "abc"
+    if d == 3:
+        return "def"
+    if d == 4:
+        return "ghi"
+    if d == 5:
+        return "jkl"
+    if d == 6:
+        return "mno"
+    if d == 7:
+        return "pqrs"
+    if d == 8:
+        return "tuv"
+    if d == 9:
+        return "wxyz"
+    return " "
+    
 
+
+def keypad(n):
+    #Implement Your Code Here
+    # pass
+    if n == 0:
+        output = []
+        output.append("")
+        return output
+    
+    smallerNumber = n//10
+    lastDigit = n%10
+    
+    smallerOutput = keypad(smallerNumber)
+    
+    output = []
+        
+    lastDigitString = getString(lastDigit)
+        
+    for sub in smallerOutput:
+        for l in lastDigitString:
+            withlastDigit =  sub + l
+            output.append(withlastDigit)
+        
+    return output
+    
+    
+
+n = int(input())
+ans = keypad(n)
+for s in ans:
+    print(s)
 
 
