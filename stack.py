616e7e826f755f5ddc25e793d99e3e052166f362
Sarthak Srivastava <sarthak.btech.ec17@iiitranchi.ac.in>
2021-01-02 21:04:53 +0530

    Update stack.py

--- stack.py
+++ stack.py
@@ -165,8 +165,64 @@ while len(arr)!= 0:
     
     
     ################  Stock Span
+	
+Stock Span
+Send Feedback
+Afzal has been working with an organization called 'Money Traders' for the past few years. The organization is into the money trading business. His manager assigned him a task. For a given array/list of stock's prices for N days, find the stock's span for each day.
+The span of the stock's price today is defined as the maximum number of consecutive days(starting from today and going backwards) for which the price of the stock was less than today's price.
+For example, if the price of a stock over a period of 7 days are [100, 80, 60, 70, 60, 75, 85], then the stock spans will be [1, 1, 1, 2, 1, 4, 6].
+Explanation:
+On the sixth day when the price of the stock was 75, the span came out to be 4, because the last 4 prices(including the current price of 75) were less than the current or the sixth day's price.
+
+Similarly, we can deduce the remaining results.
+Afzal has to return an array/list of spans corresponding to each day's stock's price. Help him to achieve the task.
+Input Format:
+The first line of input contains an integer N, denoting the total number of days.
+
+The second line of input contains the stock prices of each day. A single space will separate them.
+Output Format:
+The only line of output will print the span for each day's stock price. A single space will separate them.
+Note:
+You are not required to print the expected output explicitly. It has already been taken care of. 
+Constraints:
+0 <= N <= 10^7
+1 <= X <= 10^9
+Where X denotes the stock's price for a day.
+
+Time Limit: 1 second
+Sample Input 1:
+4
+10 10 10 10
+Sample Output 1:
+1 1 1 1 
+Sample Input 2:
+8
+60 70 80 100 90 75 80 120
+Sample Output 2:
+1 2 3 4 1 1 2 8 
+
+
+Code Pair
+
+Python (3.5)
+1
+
     
-    
+def stockSpan(price, n) :
+	stk = list()
+	output = [-1] * n
+	stk.append(0)
+	output[0] = 1
+	for i in range(1, n) :
+		while (not isEmpty(stk)) and (price[top(stk)] < price[i]) :
+			stk.pop()
+		
+		if isEmpty(stk) :
+			output[i] = i + 1
+		else :
+			output[i] = i - top(stk)
+		stk.append(i)
+	return output
     
     
     
@@ -182,6 +238,38 @@ while len(arr)!= 0:
     
     
     ###################  Minimum bracket Reversal
+	
+	
+Minimum bracket Reversal
+Send Feedback
+For a given expression in the form of a string, find the minimum number of brackets that can be reversed in order to make the expression balanced. The expression will only contain curly brackets.
+If the expression can't be balanced, return -1.
+Example:
+Expression: {{{{
+If we reverse the second and the fourth opening brackets, the whole expression will get balanced. Since we have to reverse two brackets to make the expression balanced, the expected output will be 2.
+
+Expression: {{{
+In this example, even if we reverse the last opening bracket, we would be left with the first opening bracket and hence will not be able to make the expression balanced and the output will be -1.
+Input Format :
+The first and the only line of input contains a string expression, without any spaces in between.
+Output Format :
+The only line of output will print the number of reversals required to balance the whole expression. Prints -1, otherwise.
+Note:
+You don't have to print anything. It has already been taken care of.
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the expression.
+
+Time Limit: 1sec
+Sample Input 1:
+{{{
+Sample Output 1:
+-1
+Sample Input 2:
+{{{{}}
+Sample Output 2:
+1
+
     
     
 def countMinReversals(expr):  
