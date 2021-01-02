c66f47732c811fc43457d094dbe2f0b7052f5803
Sarthak Srivastava <sarthak.btech.ec17@iiitranchi.ac.in>
2021-01-02 23:23:58 +0530

    Update DP1.py

--- DP1.py
+++ DP1.py
@@ -200,4 +200,40 @@ def lis(arr):
                 
     
     return max(l)
+  
+  
+  
+  
+  
+Minimum Number Of Squares
+Send Feedback
+A number can always be represented as a sum of squares of other numbers. Note that 1 is a square and we can always break a number as [(1 * 1) + (1 * 1) + (1 * 1) + …]. Given a number n, find the minimum number of squares that sum to n.
+Input format:
+ The first and only line of input contains an integer N (1 <= N <= 10000)
+Constraints:
+ Time Limit: 1 seconds
+Output format:
+The first and only line of output contains the minimum number if squares that sum to n.
+Sample Test Cases:
+Sample Input 1:
+100
+Sample Output 1:
+1
+Explanation:
+We can write 100 as 10^2 also, 100 can be written as (5^2) + (5^2) + (5^2) + (5^2), but this representation requires 4 squares. So, in this case, the expected answer would be 1, that is, 10^2.
+
+
+
+
+def minStepsTo1(n):
+  dp = [0 for i in range(n+1)]
+  for i in range(1,n+1):
+    dp[i] = sys.maxsize
+    for j in range(1,i+1):
+      temp = j*j
+      if temp > i:
+        break
+      dp[i] = min(dp[i],1+dp[i-temp])
+  ans = dp[n]
+  return ans
         
