7fafad2a7582ab5cf045048f93fa979cc838d34f
Sarthak Srivastava <sarthak.btech.ec17@iiitranchi.ac.in>
2021-01-03 11:27:53 +0530

    Update recurrsion2.py

--- recurrsion2.py
+++ recurrsion2.py
@@ -80,6 +80,19 @@ print(removeConsecutiveDuplicates(str))
 ##### #merge sort
 
 
+
+Algorithm	Time Complexity	 
+ 	                Best	Average	Worst	 
+Selection Sort	    Ω(n^2)	θ(n^2)	O(n^2)	 
+Bubble Sort	        Ω(n)	θ(n^2)	O(n^2)	 
+Insertion Sort	    Ω(n)	θ(n^2)	O(n^2)	 
+Heap Sort	   Ω(n log(n)) θ(n log(n))	O(n log(n))	 
+Quick Sort	   Ω(n log(n))	θ(n log(n))	O(n^2)	 
+Merge Sort	   Ω(n log(n))	θ(n log(n))	O(n log(n))	 
+Bucket Sort	      Ω(n+k)	θ(n+k)	O(n^2)	 
+Radix Sort	       Ω(nk)	θ(nk)	O(nk)
+
+
 def merge(a1,a2,arr):
     i = 0 
     j = 0
