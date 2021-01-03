4f427fbf04bc74ddd6d17fac8838ee5e8187ae9d
Sarthak Srivastava <sarthak.btech.ec17@iiitranchi.ac.in>
2021-01-03 14:18:17 +0530

    Update recurrsion3.py

--- recurrsion3.py
+++ recurrsion3.py
@@ -212,6 +212,34 @@ def subsetsSumK(arr, n, sum):
 
     
     
+def subset(arr, k):
+    n=len(arr)
+    if(n==1):
+        if(arr[0]==k):
+            output = [ [k] ]
+            return output
+        else:
+            output = []
+            return output
+
+    output1 = subset(arr[:-1], k)
+    output2 = subset(arr[:-1], k-arr[-1])
+    for lst in output2:
+        lst.append(arr[-1])
+    if arr[-1]==k:
+        output2.append([k])
+    return output1 + output2
+
+n=int(input())
+arr=list(int(i) for i in input().strip().split(' '))
+k=int(input())
+output=subset(arr, k)
+for lst in output:
+    print(*lst)
+
+
+    
+    
     Return subset of an array
 Send Feedback
 Given an integer array (of length n), find and return all the subsets of input array.
