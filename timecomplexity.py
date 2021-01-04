66334a45bf22833a84bed74e7471f54b565a6d34
Sarthak Srivastava <sarthak.btech.ec17@iiitranchi.ac.in>
2021-01-04 11:53:53 +0530

    Update timecomplexity.py

--- timecomplexity.py
+++ timecomplexity.py
@@ -287,42 +287,45 @@ For the second query, we have 2 pairs in total that sum up to 10. They are, (2, 
 
 
 def pairSum(arr, n, num) :
-  arr.sort()
-  
-  startIndex = 0
-  endIndex = (n - 1)
-  
-  numPair = 0
-  
-  while startIndex < endIndex :
-  
-    if arr[startIndex] + arr[endIndex] < num :
-      startIndex += 1
-    elif arr[startIndex] + arr[endIndex] > num :
-      endIndex -= 1
-    else :
-      elementAtStart = arr[startIndex]
-      elementAtEnd = arr[endIndex]
-      if elementAtStart == elementAtEnd :
-        totalElementsFromStartToEnd = (endIndex - startIndex) + 1
-        numPair += (totalElementsFromStartToEnd * (totalElementsFromStartToEnd - 1) // 2)
-        return numPair
-      
-      tempStartIndex = startIndex + 1
-      tempEndIndex = endIndex - 1
-      
-      while (tempStartIndex <= tempEndIndex) and (arr[tempStartIndex] == elementAtStart) :
-        tempStartIndex += 1
-      while (tempEndIndex >= tempStartIndex) and (arr[tempEndIndex] == elementAtEnd) :
-        tempEndIndex -= 1
-      totalElementsFromStart = (tempStartIndex - startIndex)
-      totalElementsFromEnd = (endIndex - tempEndIndex)
-      numPair += (totalElementsFromStart * totalElementsFromEnd)
-      startIndex = tempStartIndex
-      endIndex = tempEndIndex
-  
-  return numPair
-
+	#Your code goes here
+    arr.sort()
+    startIndex = 0
+    endIndex = (n - 1)
+    
+    numPair = 0
+    
+    while startIndex < endIndex :
+        if arr[startIndex] + arr[endIndex] < num :
+            startIndex += 1
+            
+        elif arr[startIndex] + arr[endIndex] > num :
+            endIndex -= 1
+            
+        else :
+            elementAtStart = arr[startIndex]
+            elementAtEnd = arr[endIndex]
+            if elementAtStart == elementAtEnd :
+                totalElementsFromStartToEnd = (endIndex - startIndex) + 1
+                numPair += (totalElementsFromStartToEnd * (totalElementsFromStartToEnd - 1) // 2)
+                return numPair
+            
+            tempStartIndex = startIndex + 1
+            tempEndIndex = endIndex - 1
+            
+            while (tempStartIndex <= tempEndIndex) and (arr[tempStartIndex] == elementAtStart) :
+                tempStartIndex += 1
+                
+            while (tempEndIndex >= tempStartIndex) and (arr[tempEndIndex] == elementAtEnd) :
+                
+                tempEndIndex -= 1
+                
+            totalElementsFromStart = (tempStartIndex - startIndex)
+            totalElementsFromEnd = (endIndex - tempEndIndex)
+            numPair += (totalElementsFromStart * totalElementsFromEnd)
+            startIndex = tempStartIndex
+            endIndex = tempEndIndex
+            
+    return numPair
 
 
 
