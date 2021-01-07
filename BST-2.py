bea67ebee43cce5a914f2a416e0c7935f8244e2b
Sarthak Srivastava <sarthak.btech.ec17@iiitranchi.ac.in>
2021-01-07 12:51:34 +0530

    Update BST-2.py

--- BST-2.py
+++ BST-2.py
@@ -98,6 +98,32 @@ def findPathBST(root,data):
 # There is no space in between.
 # You need to print all nodes in the recursive format in different lines.
 
+
+########3delete
+
+1) Node to be deleted is leaf: Simply remove from the tree. 
+
+              50                            50
+           /     \         delete(20)      /   \
+          30      70       --------->    30     70 
+         /  \    /  \                     \    /  \ 
+       20   40  60   80                   40  60   80
+2) Node to be deleted has only one child: Copy the child to the node and delete the child 
+
+              50                            50
+           /     \         delete(30)      /   \
+          30      70       --------->    40     70 
+            \    /  \                          /  \ 
+            40  60   80                       60   80
+3) Node to be deleted has two children: Find inorder successor of the node. Copy contents of the inorder successor to the node and delete the inorder successor. Note that inorder predecessor can also be used. 
+ 
+
+              50                            60
+           /     \         delete(50)      /   \
+          40      70       --------->    40    70 
+                 /  \                            \ 
+                60   80                           80
+The important thing to note is, inorder successor is needed only when right child is not empty. In this particular case, inorder successor can be obtained by finding the minimum value in right child of the node.
     
 class BinaryTreeNode:
     def __init__(self, data):
