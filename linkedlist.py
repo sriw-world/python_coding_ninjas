f064efd81be1e56da946500f93190b0b6107c369
Sarthak Srivastava <sarthak.btech.ec17@iiitranchi.ac.in>
2021-01-20 11:43:30 +0530

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -780,6 +780,35 @@ def kReverse(head, k) :
     
     return newhead
     
+	
+	
+	
+	
+	
+############alt soln
+def reverse(self, head, k): 
+        current = head  
+        next  = None
+        prev = None
+        count = 0 
+          
+        # Reverse first k nodes of the linked list 
+        while(current is not None and count < k): 
+            next = current.next
+            current.next = prev 
+            prev = current 
+            current = next 
+            count += 1
+  
+        # next is now a pointer to (k+1)th node 
+        # recursively call for the list starting 
+        # from current. And make rest of the list as 
+        # next of first node 
+        if next is not None: 
+            head.next = self.reverse(next, k) 
+  
+        # prev is new head of the input list 
+        return prev 
         
 # Bubble Sort (Iterative) LinkedList
 
