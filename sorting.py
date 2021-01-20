870acc0169759fdb1570e7de75b3a21bd8861ad5
Sarthak Srivastava <sarthak.btech.ec17@iiitranchi.ac.in>
2021-01-20 19:02:22 +0530

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -91,3 +91,51 @@ for i in range(len(A)):
 print ("Sorted array") 
 for i in range(len(A)): 
     print("%d" %A[i])
+
+    
+    
+
+insertion-sort  Algorithm
+To sort an array of size n in ascending order:
+1: Iterate from arr[1] to arr[n] over the array.
+2: Compare the current element (key) to its predecessor.
+3: If the key element is smaller than its predecessor, compare it to the elements before. Move the greater elements one position up to make space for the swapped element.
+
+
+Another Example:
+12, 11, 13, 5, 6
+
+Let us loop for i = 1 (second element of the array) to 4 (last element of the array)
+
+i = 1. Since 11 is smaller than 12, move 12 and insert 11 before 12
+11, 12, 13, 5, 6
+
+i = 2. 13 will remain at its position as all elements in A[0..I-1] are smaller than 13
+11, 12, 13, 5, 6
+
+i = 3. 5 will move to the beginning and all other elements from 11 to 13 will move one position ahead of their current position.
+5, 11, 12, 13, 6
+
+i = 4. 6 will move to position after 5, and elements from 11 to 13 will move one position ahead of their current position.
+5, 6, 11, 12, 13
+
+
+def insertionSort(arr): 
+  
+    # Traverse through 1 to len(arr) 
+    for i in range(1, len(arr)): 
+  
+        key = arr[i] 
+  
+        # Move elements of arr[0..i-1], that are 
+        # greater than key, to one position ahead 
+        # of their current position 
+        j = i-1
+        while j >= 0 and key < arr[j] : 
+                arr[j + 1] = arr[j] 
+                j -= 1
+        arr[j + 1] = key 
+  
+  
+# Driver code to test above 
+arr = [12, 11, 13, 5, 6] 
