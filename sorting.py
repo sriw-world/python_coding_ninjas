3bebe96aa01202ff12b6ccc9a7fefdf704022e7c
Sarthak Srivastava <sarthak.btech.ec17@iiitranchi.ac.in>
2021-01-20 18:42:37 +0530

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -14,3 +14,11 @@ def bubbleSort(arr):
             # than the next element 
             if arr[j] > arr[j+1] : 
                 arr[j], arr[j+1] = arr[j+1], arr[j] 
+
+arr = [64, 34, 25, 12, 22, 11, 90] 
+  
+bubbleSort(arr) 
+  
+print ("Sorted array is:") 
+for i in range(len(arr)): 
+    print ("%d" %arr[i])
